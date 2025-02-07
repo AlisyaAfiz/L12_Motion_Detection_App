0d6955076f4a8e1fede8614512c2684e10af7ad4
AlisyaAfiz <23018913@myrp.edu.sg>
2025-02-07 14:59:00 +0800

    Updates

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState, useEffect } from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { Audio } from "expo-av";
+import { Gyroscope } from "expo-sensors";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    shakeText: {
+        fontSize: 90,
+        textAlign: "center",
+        marginTop: 50
+    }
 });
+
+export default function App() {
+    const [isShaking, setIsShaking] = useState(false);
+
+    async function playSound() {
+        const { sound } = await Audio.Sound.createAsync(require("./shaker.wav"));
+        await sound.playAsync();
+    }
+
+    useEffect(() => {
+        Gyroscope.setUpdateInterval(100);
+        const subscription = Gyroscope.addListener(({ x, y, z }) => {
+            if (Math.abs(x) > 1.5 || Math.abs(y) > 1.5 || Math.abs(z) > 1.5) {
+                if (!isShaking) {
+                    setIsShaking(true);
+                    playSound();
+                }
+            } else {
+                setIsShaking(false);
+            }
+        });
+
+        return () => subscription.remove();
+    }, [isShaking]);
+
+    return (
+        <View>
+            <StatusBar />
+            {isShaking && <Text style={styles.shakeText}>SHAKE!</Text>}
+        </View>
+    );
+}
